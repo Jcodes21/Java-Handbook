ea4a0db750d6af7831e8f9e291a522a8b80d1b55
Joseph halden <josephhalden1@gmail.com>
2026-04-30 22:22:24 +0100

    Refactor StringMethods class and update string usage

--- StringMethods.java
+++ StringMethods.java
@@ -1,13 +1,15 @@
 //https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/lang/String.html
-
-public class phrase {
+public class Strings {
     public static void main(String a[]){
-        String phrase = "Play with me";
+        char blocks[]={'a','l','!'};
+        String Str = new String(blocks);
+        String phrase = "I'm a str1ng. Play with me";
         String wSpace = "  ";
-        System.out.println(phrase.toUpperCase());  
+        System.out.print(Str);
+        System.out.println(Str.toUpperCase());  
         System.out.println(phrase.toLowerCase());  
-        System.out.println(phrase.isEmpty());  
+        System.out.println("Is empty?"+phrase.isEmpty());  
         System.out.println(wSpace.isBlank());  
-        System.out.println(phrase.substring(1,2));  
+        System.out.println(phrase.substring(1,2));
     }
 }
