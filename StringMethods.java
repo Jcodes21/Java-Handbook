fb8b4ce8112ffdc094c61df18d4962081465da46
Joseph halden <josephhalden1@gmail.com>
2026-05-01 10:39:15 +0100

    Enhance StringMethods with trim and strip methods

--- StringMethods.java
+++ StringMethods.java
@@ -5,6 +5,10 @@ public class StringMethods {
         String Str = new String(blocks);
         String phrase = "I'm a str1ng. Play with me";
         String wSpace = "  ";
+        String trimMe = "Spaces gone?     ";
+        String trimMe2 = "     Spaces gone?     ";
+
+        
 
         System.out.println(phrase + "!");
         System.out.println(phrase.length());
@@ -15,11 +19,17 @@ public class StringMethods {
        // System.out.println(phrase.indexOf());
        //  System.out.println(phrase.lastIndexOf());
         // System.out.println(phrase.replace());
-        System.out.print(Str);
+        System.out.println(Str);
+        System.out.println(phrase.toCharArray());
         System.out.println(Str.toUpperCase());  
         System.out.println(phrase.toLowerCase());  
         System.out.println("Is empty?"+phrase.isEmpty());  
         System.out.println(wSpace.isBlank());  
         System.out.println(phrase.substring(1,2));
+        System.out.println(trimMe.trim());
+        System.out.println(trimMe2.strip());
+        System.out.println(trimMe2.stripLeading());
+        System.out.println(trimMe2.stripTrailing());
+        
     }
 }
